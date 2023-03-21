1fa3b6780a50d3c2a62eb33f2d41a38407072f9b
khaled492007 <khmo492007@gmail.com>
2023-03-21 15:26:40 +0200

    i

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_page(reqest):
+
+    return HttpResponse("Hallo,this's first project of django")
